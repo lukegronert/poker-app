0a3f6d24b5d4bb2d0a33fbcad3e4e0f923d7875f
Luke G <lukegronert@gmail.com>
2022-01-03 21:51:33 +0800

    Added netlify-identity-widget and added it to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import netlifyIdentity from 'netlify-identity-widget'
+
+netlifyIdentity.init();
 
 ReactDOM.render(
   <React.StrictMode>
