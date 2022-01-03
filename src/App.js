623d4c8d6df5b9097acf22d411e62a2469921e22
Luke G <lukegronert@gmail.com>
2022-01-03 22:25:04 +0800

    NetlifyIdentity now works on page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import {useEffect} from 'react';
+
+// Import Netlify Identity script into HTML of site
+// Must be called from React life-cycle function
+// Will not work if called at build-time
+function initNetlifyIdentity() {
+  console.log('netlify identity called')
+  const script = document.createElement('script');
+
+  script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
+  script.async = true;
+
+  document.body.appendChild(script);
+}
+
+function openNetlifyModal() {
+  const netlifyIdentity = window.netlifyIdentity;
+
+  if(netlifyIdentity) {
+    netlifyIdentity.open();
+  } else {
+    console.log('netlifyIdentity not defined')
+  }
+}
+
 function App() {
+  useEffect(() => {
+    initNetlifyIdentity();
+  })
+  
   return (
     <div className="App">
       <h1>POKER STUFF</h1>
+      <button onClick={() => openNetlifyModal()}>Login/Sign Up</button>
     </div>
   );
 }
