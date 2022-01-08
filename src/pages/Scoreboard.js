fd4abc5641deca7081091caa29db31af69e4a70b
Luke G <lukegronert@gmail.com>
2022-01-08 12:50:56 +0800

    Got google-spreadsheet to function, env variables to work. Ready to start setting up google-spreadsheet

--- src/pages/Scoreboard.js
+++ src/pages/Scoreboard.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import EntryForm from '../components/EntryForm';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+
+const { 
+    REACT_APP_SHEET_ID,
+    REACT_APP_GOOGLE_CLIENT_EMAIL,
+    REACT_APP_GOOGLE_PRIVATE_KEY
+    } = process.env;
+const doc = new GoogleSpreadsheet(`${process.env.REACT_APP_SHEET_ID}`);
+console.log(process.env.REACT_APP_SHEET_ID)
 
 export default function Scoreboard() {
     return (
